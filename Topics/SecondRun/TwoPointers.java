a8e5f97889a6f6090677f8549aba23083a9be78d
Xianwen Peng <fallenpxw@gmail.com>
2018-08-18 02:22:19 -0400

    Update Notes

--- Topics/SecondRun/TwoPointers.java
+++ Topics/SecondRun/TwoPointers.java
@@ -3,6 +3,46 @@ import java.util.*;
 
 public class TwoPointers {
 
+    /* 65. Median of two Sorted Arrays */
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        // write your code here
+        if (A.length > B.length) {
+            return findMedianSortedArrays(B, A);
+        }
+        if (A.length == 0)
+            return B.length % 2 != 0 ? B[B.length / 2]
+                    : (B[B.length / 2] + B[B.length / 2 - 1]) / 2.0;
+
+        int x = A.length;
+        int y = B.length;
+
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int partitionX = (low + high)/2;
+            int partitionY = (x + y + 1)/2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : A[partitionX - 1];
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : B[partitionY - 1];
+
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : A[partitionX];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : B[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return (double)Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX <= minRightY) {
+                low = partitionX + 1;
+            } else {
+                high = partitionX - 1;
+            }
+        }
+        return -1;
+    }
+
     /* 884. Decoded String at Index */
     public String decodeAtIndex(String S, int K) {
         long len = 0;
